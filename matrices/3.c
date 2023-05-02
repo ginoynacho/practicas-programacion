f48d7807a03f3b239b7a4a30cb6fd304321172e9
ginoynacho <127965100+ginoynacho@users.noreply.github.com>
2023-05-02 12:29:45 -0300

    Update 3.c

--- matrices/3.c
+++ matrices/3.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+void BusacarMayor(float arreglo_numeros[][5],  int filas);
 int main()
 {
     int filas, columnas;
     printf("filas \n");
     scanf(" %d", &filas);
-    printf("columnas \n");
-    scanf(" %d", &columnas);
-    float num[filas][columnas];
-    for(int cont_columnas = 0; cont_columnas < columnas; cont_columnas++){
+    float arreglo_numeros[filas][5];
+    for(int cont_columnas = 0; cont_columnas < 5; cont_columnas++){
         for( int cont_filas = 0; cont_filas < filas; cont_filas++){
             printf("Numero ");
-            scanf("%f", &num[cont_filas][cont_columnas]);
+            scanf("%f", &arreglo_numeros[cont_filas][cont_columnas]);
         }
     }
+    BusacarMayor( arreglo_numeros, filas);
+}
+
+void BusacarMayor(float arreglo_numeros[][5], int filas){
     float num_mayor;
-    for(int cont_columnas = 0; cont_columnas < columnas; cont_columnas++){
+    for(int cont_columnas = 0; cont_columnas < 5; cont_columnas++){
         for(int cont_filas = 0; cont_filas<filas; cont_filas++){
           if(cont_columnas == 0  && cont_filas == 0){
-            num_mayor = num[cont_filas][cont_columnas];
+            num_mayor = arreglo_numeros[cont_filas][cont_columnas];
           }
-          if(num[cont_filas][cont_columnas] > num_mayor){
-                num_mayor = num[cont_filas][cont_columnas];
+          if(arreglo_numeros[cont_filas][cont_columnas] > num_mayor){
+                num_mayor = arreglo_numeros[cont_filas][cont_columnas];
             }
         }
 }
     printf("Numero mayor %.2f", num_mayor);
+
+
 }
